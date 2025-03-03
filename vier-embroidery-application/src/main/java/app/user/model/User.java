228d5b33c953bd8aa10bbd2a9ed0ec70c10b08fc
Tony Todorov <tonytodorov@protonmail.com>
2025-03-03 12:25:28 +0200

    Refactor user model

--- vier-embroidery-application/src/main/java/app/user/model/User.java
+++ vier-embroidery-application/src/main/java/app/user/model/User.java
@@ -23,16 +23,22 @@ public class User {
     private UUID id;
 
     @Column(nullable = false, unique = true)
-    private String username;
-
-    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
     private String password;
 
     @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
     private String phoneNumber;
+
+    @Column
+    private String address;
 
     @Column
     @Enumerated(EnumType.STRING)
